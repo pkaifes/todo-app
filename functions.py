10027534fffa78763023d52098a3268209f693f6
Parker <pkaifes@gmail.com>
2025-01-04 17:06:01 -0600

    add list-box that shows todos, edit button, and make the todos box update

--- functions.py
+++ functions.py
@@ -10,16 +10,17 @@ def get_todos(filepath=FILEPATH):
     """Retrieves to-do list from default file"""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
+        #todos_local = [todo.strip() for todo in todos_local]
     return todos_local
 
 
-def write_todos(todo_list):
+def write_todos(todo_list, filepath=FILEPATH):
     """save to-do list to default file"""
     #save and close to datasource
-    todo_list = "\n".join(todo_list)
-    file = open("todos.txt","w")
-    file.writelines(todo_list)
-    file.close()
+    #todo_list = "\n".join(todo_list)
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todo_list)
+
 
 
 if __name__ == "__main__":
--- main_gui.py
+++ main_gui.py
@@ -5,11 +5,15 @@ import FreeSimpleGUI as FSGUI
 lbl_Intro = FSGUI.Text("Type in a to-do")
 inp_Todo = FSGUI.InputText(tooltip="Enter todo", key="todo")
 btn_add = FSGUI.Button("Add")
-
-
+lst_todos = FSGUI.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
+btn_edit = FSGUI.Button("Edit")
 
 window = FSGUI.Window('My To-Do App',
-                      layout=[[lbl_Intro], [inp_Todo, btn_add]],
+                      layout=[
+                          [lbl_Intro],
+                          [inp_Todo, btn_add],
+                          [lst_todos, btn_edit]
+                      ],
                       font=("Helvetica", 20)
                       )
 
@@ -22,9 +26,25 @@ while True:
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_Todo = values["todo"]
+            new_Todo = values["todo"] + "\n"
             todos.append(new_Todo)
             functions.write_todos(todos)
+            window["todos"].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_Todo = values["todo"]
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_Todo + "\n"
+            functions.write_todos(todos)
+
+            window["todos"].update(values=todos)
+
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
+            #print("seen todos")
         case FSGUI.WIN_CLOSED:
             break
 
